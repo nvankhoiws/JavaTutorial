6b457ba0ca248f67538667ad4d69bef8fa69789e
Nguyen Van Khoi <nvankhoiws@gmai.com>
2017-10-17 16:00:25 +0700

    Polymorphism challenge exercise 1

--- src/core/com/udemy/timbuchalka/encapsulation/Printer.java
+++ src/core/com/udemy/timbuchalka/encapsulation/Printer.java
@@ -4,12 +4,11 @@ package core.com.udemy.timbuchalka.encapsulation;
  * Created by Khoi.NguyenVan@nttdata.com on 10/16/20175:46 PM.
  */
 public class Printer {
-    private int tonerLevel;
+    private double tonerLevel;
     private int accumulativePagePrinted = 1000;
     private int currentPagePrinted;
-    private boolean duplexPriting;
 
-    public Printer(int tonerLevel, int accumulativePagePrinted, boolean duplexPriting) {
+    public Printer(double tonerLevel, int accumulativePagePrinted) {
         if (tonerLevel > 0 && tonerLevel <=100) {
             this.tonerLevel = tonerLevel;
         } else if (tonerLevel <= 0) {
@@ -24,46 +23,58 @@ public class Printer {
         } else {
             this.accumulativePagePrinted = accumulativePagePrinted;
         }
-        this.duplexPriting = duplexPriting;
     }
 
-    public void fillUpToner(int percent){
+    public void fillUpToner(double percent){
         if (percent > 0 && percent <= 100 - this.tonerLevel) {
             this.tonerLevel = this.tonerLevel + percent;
-            System.out.println("Printer is filled up to " + this.tonerLevel + "%");
+            System.out.println("Toner is filled up to " + this.tonerLevel + "%");
         } else if (percent > 0 && percent > 100 - this.tonerLevel) {
             this.tonerLevel = 100;
-            System.out.println("Printer is filled up to 100%");
+            System.out.println("Toner is filled up to 100%");
         } else {
             this.tonerLevel = 0;
-            System.out.println("Printer is not filled up yet");
+            System.out.println("Toner is not filled up yet");
         }
     }
 
-    public int getTonerLevel() {
+    public double getTonerLevel() {
         if (this.tonerLevel <= 0) {
-            System.out.println("Printer is emptied out");
+            System.out.println("Toner is emptied out");
         } else {
-            System.out.println("Printer is remained at " + this.tonerLevel + "%");
+            System.out.println("Toner remains at " + this.tonerLevel + "%");
         }
         return tonerLevel;
     }
 
-    public void checkDuplexPrinting(){
-        if (this.duplexPriting) {
-
-        }
+    public void printing(int pageNumber){
+        System.out.println("Printer is printing...");
+        System.out.println("================================Report================================");
+        setTonerLevel(this.tonerLevel - pageNumber/250);
+        System.out.println("Toner level remains at " + this.tonerLevel + "%");
+        setCurrentPagePrinted(pageNumber);
+        System.out.println("Pages are printed is " + pageNumber);
+        setAccumulativePagePrinted(getAccumulativePagePrinted() + pageNumber);
+        System.out.println("Accumulative page printed until now is " + getAccumulativePagePrinted());
     }
+
     public int getAccumulativePagePrinted() {
         return accumulativePagePrinted;
     }
 
     public int getCurrentPagePrinted() {
-        this.accumulativePagePrinted = this.accumulativePagePrinted + this.currentPagePrinted;
         return currentPagePrinted;
     }
 
-    public boolean isDuplexPriting() {
-        return duplexPriting;
+    private void setTonerLevel(double tonerLevel) {
+        this.tonerLevel = tonerLevel;
+    }
+
+    private void setAccumulativePagePrinted(int accumulativePagePrinted) {
+        this.accumulativePagePrinted = accumulativePagePrinted;
+    }
+
+    private void setCurrentPagePrinted(int currentPagePrinted) {
+        this.currentPagePrinted = currentPagePrinted;
     }
 }
