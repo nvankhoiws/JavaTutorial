5f9efe4941de0a34d1b92aced3038433a213209b
Nguyen Van Khoi <nvankhoiws@gmai.com>
2017-12-01 18:02:26 +0700

    add isodd.java

---
+++ src/src/core/self/arithmetic/IsOdd.java
@@ -0,0 +1,16 @@
+package src.core.self.arithmetic;
+
+/**
+ * Created by Khoi.NguyenVan@nttdata.com on 12/1/201711:46 AM.
+ */
+public class IsOdd {
+
+    public static void main(String[] args) {
+        System.out.println(String.valueOf(isOdd(-4)));
+    }
+
+    public static boolean isOdd (int i ){
+        return (i % 2) != 0;
+    }
+
+}
