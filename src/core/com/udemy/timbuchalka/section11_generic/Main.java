0a3d27018294821c3ba65c3a57d1a87b235025fa
Nguyen Van Khoi <nvankhoiws@gmai.com>
2017-12-21 17:58:03 +0700

    add

--- src/core/com/udemy/timbuchalka/section11_generic/Main.java
+++ src/core/com/udemy/timbuchalka/section11_generic/Main.java
@@ -1,5 +1,10 @@
 package core.com.udemy.timbuchalka.section11_generic;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -14,9 +19,8 @@ public class Main {
 //        hanoiTT.addPlayer(vanKieu);
 //        hanoiTT.addPlayer(caoKeu);
 
-
         // What problem when we use any kind of data like this Team<String> ?
-        Team<String> brokenTeam = new Team<>("broken-team");
+//        Team<String> brokenTeam = new Team<>("broken-team");
 //        brokenTeam.addPlayer("NguyenVanKhoi");
 
         // Implement the matchResult method in Team class ?
@@ -24,10 +28,29 @@ public class Main {
         ximangHaiphong.matchResult(hanoiTT, 4, 2);
 
         // How to a football team fight with a basket ball team ? How to prevent that
-        Team<BasketBallPlayer> basketBallTeam = new Team<>("Buffulo");
-        basketBallTeam.matchResult(ximangHaiphong, 7, 2);
+        Team<BasketBallPlayer> goldenBuffulo = new Team<>("Golden Buffulo");
+//        basketBallTeam.matchResult(ximangHaiphong, 7, 2);
+        Team<BasketBallPlayer> childFomVillage = new Team<>("Child from Village");
 
+        goldenBuffulo.matchResult(childFomVillage, 7, 2);
+
+        Team<BasketBallPlayer> boyNhaNgheo = new Team<>("Ngheo qua khong biet lam gi");
+
+        goldenBuffulo.matchResult(boyNhaNgheo, 4, 4);
+
+        System.out.println(goldenBuffulo.getName() + " has got " + goldenBuffulo.ranking());
+        System.out.println(childFomVillage.getName() + " has got " + childFomVillage.ranking());
+        System.out.println(boyNhaNgheo.getName() + " has got " + boyNhaNgheo.ranking());
+
+        List<Team> teamlist = new ArrayList<>();
+        teamlist.add(goldenBuffulo);
+        teamlist.add(ximangHaiphong);
+        teamlist.add(childFomVillage);
+
+        Collections.sort(teamlist);
+
+        for (int i = 0, length = teamlist.size(); i < length; i++) {
+            System.out.println(teamlist.get(i).getName() + " is ranked at " + (i+1) + " " + teamlist.get(i).ranking());
+        }
     }
-
-
 }
