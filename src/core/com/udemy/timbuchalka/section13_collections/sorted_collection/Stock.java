54100b985b6caa5269216629d3ca577c1a0308d9
Nguyen Van Khoi <nvankhoiws@gmail.com>
2018-01-08 22:44:27 +0700

    add stock list

---
+++ src/core/com/udemy/timbuchalka/section13_collections/sorted_collection/Stock.java
@@ -0,0 +1,16 @@
+package core.com.udemy.timbuchalka.section13_collections.sorted_collection;
+
+import java.util.Map;
+
+public class Stock {
+    private Map<String, StockItem> list;
+
+    public StockItem addStock(StockItem item){
+
+    }
+
+    public int sellStock(String item, int stockQuantity) {
+        
+    }
+
+}
