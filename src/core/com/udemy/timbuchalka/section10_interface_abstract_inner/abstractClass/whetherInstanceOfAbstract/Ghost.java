0127e37438a51a377dccce9921070bb3636a6821
Nguyen Van Khoi <nvankhoiws@gmail.com>
2018-07-29 17:16:58 +0700

    Add example of instance of abstract class

---
+++ src/core/com/udemy/timbuchalka/section10_interface_abstract_inner/abstractClass/whetherInstanceOfAbstract/Ghost.java
@@ -0,0 +1,5 @@
+package core.com.udemy.timbuchalka.section10_interface_abstract_inner.abstractClass.whetherInstanceOfAbstract;
+
+public abstract class Ghost {
+
+}
---
+++ src/core/com/udemy/timbuchalka/section10_interface_abstract_inner/abstractClass/whetherInstanceOfAbstract/Main.java
@@ -0,0 +1,10 @@
+package core.com.udemy.timbuchalka.section10_interface_abstract_inner.abstractClass.whetherInstanceOfAbstract;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Ghost ghost = new Ghost(); // Cannot instantiate init of ghost class
+	}
+
+}
