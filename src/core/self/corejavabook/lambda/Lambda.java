1b615bb3f6f201f536ffb0819ffa2855ff544791
Nguyen Van Khoi <nvankhoiws@gmail.com>
2018-10-02 13:40:32 +0700

    Add lambda class

---
+++ src/core/self/corejavabook/lambda/Lambda.java
@@ -0,0 +1,15 @@
+package core.self.corejavabook.lambda;
+
+import java.util.Arrays;
+
+public class Lambda {
+	public static void main(String[] args) {
+		String[] arrStr = {"Khoi", "PhuongDung"};
+		Arrays.sort(arrStr,
+				(str1, str2) ->
+						- str1.length() + str2.length());
+		for (String str : arrStr) {
+			System.out.println(str);
+		}
+	}
+}
