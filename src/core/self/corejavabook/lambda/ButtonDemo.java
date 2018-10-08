89513bcbec7040c506378d89db50de62c01cc900
Nguyen Van Khoi <nvankhoiws@gmail.com>
2018-10-08 16:00:40 +0700

    new commit

---
+++ src/core/self/corejavabook/lambda/ButtonDemo.java
@@ -0,0 +1,51 @@
+package core.self.corejavabook.lambda;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+// From the command line, you can run this program as
+// java ch03.sec04.ButtonDemo
+// even though it has no main method
+
+public class ButtonDemo extends Application {
+	public void start(Stage stage) {
+
+		Button button = new Button("Ok");
+
+		/**
+		 * block code 1
+		 * This block code equals with block code 2
+		 */
+		//        button.setOnAction(e ->  {
+//            System.out.println(e instanceof EventHandler);
+//        });
+
+		/**
+		 * start: block code 2
+		 */
+		button.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println(event instanceof ActionEvent);
+			}
+		});
+		/**
+		 * end: block code 2
+		 */
+
+		Label message = new Label("Hello, JavaFX!");
+		message.setFont(new Font(100));
+		VBox root = new VBox();
+		root.getChildren().addAll(button, message);
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+}
---
+++ src/core/self/corejavabook/lambda/LambdaDemo.java
@@ -0,0 +1,31 @@
+package core.self.corejavabook.lambda;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.Predicate;
+
+public class LambdaDemo {
+    public static void main(String[] args) {
+        String[] friends = { "Peter", "Paul", "Mary" };
+        Arrays.sort(friends, (_1, _2) -> _1.length() - _2.length());
+        System.out.println(Arrays.toString(friends));
+        ArrayList<String> enemies = new ArrayList<>(Arrays.asList("Malfoy", "Crabbe", "Goyle", null));
+
+        /**
+         * block code 1: use lambda expression
+         */
+        enemies.removeIf(e -> e == null);
+
+        /**
+         * block code 2: tradditional way
+         */
+        enemies.removeIf(new Predicate<String>() {
+            @Override
+            public boolean test(String s) {
+                return s == null || s.equals("Goyle") || s.equals("Crabbe") || s.equals("Malfoy"); // remove all thing, haha
+            }
+        });
+
+        System.out.println(enemies);
+    }
+}
