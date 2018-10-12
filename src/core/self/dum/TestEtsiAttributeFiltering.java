675cedde6a832f81cbcd6ba0b0c5126189b24f8d
Nguyen Van Khoi <nvankhoiws@gmail.com>
2018-10-12 17:14:37 +0700

    newcommit

--- src/core/self/dum/TestEtsiAttributeFiltering.java
+++ src/core/self/dum/TestEtsiAttributeFiltering.java
@@ -3,7 +3,7 @@ package core.self.dum;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-public class EtsiAttributeFiltering {
+public class TestEtsiAttributeFiltering {
 	public static void main(String[] args) throws URISyntaxException {
 		String fakeUri = "http://localhost:8080/container?filter=(eq,parts/id,3)&&otherParam=4.5";
 		URI uri = new URI(fakeUri);
